fe55df646875c0ed5dd9f24aa26e9dbca3348415
Kai Yin Ng <kng0919@terpmail.umd.edu>
2025-04-24 11:12:16 -0400

    First commit

---
+++ Js/06_types/main.js
@@ -0,0 +1,15 @@
+var age = 20;
+
+var name = "Kai";
+
+var flag_true = true;
+var flag_false = false;
+
+var users = {
+    age:18,
+    name:"Jack",
+    marriage: false,
+}
+
+var a = null;
+var b = undefined;
---
+++ Js/08_comparison/main.js
@@ -0,0 +1,16 @@
+var num1 = 10;
+var num2 = 10;
+
+console.log(num1 > num2); //false
+
+// == === != !==
+
+var num3 = "10";
+var num4 = 10;
+
+console.log(num3 == num4);//true
+console.log(num3 === num4); //false
+
+
+console.log(num3 != num4); //flase
+console.log(num3 !== num4);//true
